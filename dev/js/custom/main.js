3424bb674d81d6ccf1443d18b7d2277a458e690e
forest-shadow <mamasay@yandex.ru>
2017-01-23 02:59:10 +0300

    js indentation fix

--- dev/js/custom/main.js
+++ dev/js/custom/main.js
@@ -1,9 +1,9 @@
 $( document ).ready( function(){
 
-    // initialize top navigation
-    $( "#top-nav" ).menumaker({
-        title: "Меню",
-        format: "multitoggle"
-    });
+  // initialize top navigation
+  $( "#top-nav" ).menumaker( {
+    title: "Меню",
+    format: "multitoggle"
+  } );
 
 });
